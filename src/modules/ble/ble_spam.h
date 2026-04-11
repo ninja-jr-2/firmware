eee6fa2db59de952f7320cf8d25e46cc18782ae3
Ninja-jr <ninjajr1983@gmail.com>
2026-04-11 19:25:27 +0200

    Refactor ble_spam.h with new spam state machines
    Refactor BLE spam header to include additional spam state machines and improve structure.

--- src/modules/ble/ble_spam.h
+++ src/modules/ble/ble_spam.h
@@ -1,16 +1,42 @@
-#pragma once
+#ifndef BLE_SPAM_H
+#define BLE_SPAM_H
 
 #include <Arduino.h>
-#include <NimBLEBeacon.h>
 #include <NimBLEDevice.h>
-#include <NimBLEServer.h>
-#include <NimBLEUtils.h>
+#include <NimBLEAdvertisedDevice.h>
+
+enum EBLEPayloadType { Microsoft, SourApple, AppleJuice, Samsung, Google };
+
+void generateRandomMac(uint8_t *mac);
+void executeSpam(EBLEPayloadType type, String customName = "");
+void executeCustomSpam(String spamName);
+void ibeacon(const char *DeviceName, const char *BEACON_UUID, int ManufacturerId);
 void aj_adv(int ble_choice);
+void legacySubMenu();
 void spamMenu();
 
-void ibeacon(
-    const char *DeviceName = "Bruce iBeacon", const char *BEACON_UUID = "8ec76ea3-6668-48da-9866-75be8bc86f4d",
-    int ManufacturerId = 0x4C00
-);
+// Legacy Apple spam state machine
+void startLegacyAppleSpam(EBLEPayloadType type);
+void stopLegacyAppleSpam();
+bool isLegacyAppleSpamRunning();
+void updateLegacyAppleSpam();
 
-#include "apple_spam.h"
+// Samsung spam state machine
+void startSamsungSpam();
+void stopSamsungSpam();
+bool isSamsungSpamRunning();
+void updateSamsungSpam();
+
+// Google FastPair spam state machine
+void startGoogleSpam();
+void stopGoogleSpam();
+bool isGoogleSpamRunning();
+void updateGoogleSpam();
+
+// Windows SwiftPair spam state machine
+void startWindowsSpam();
+void stopWindowsSpam();
+bool isWindowsSpamRunning();
+void updateWindowsSpam();
+
+#endif
